d8b755b917acf9147c9e51586addb4c0016c50f5
Caio Rodrigues <contatocerd7@gmail.com>
2025-02-25 14:02:46 -0300

    Refining user interaction code - 0.1

--- src/main/java/org/cerd/bank/controller/UserController.java
+++ src/main/java/org/cerd/bank/controller/UserController.java
@@ -16,28 +16,34 @@ public class UserController extends AccountService {
     }
 
     private void receiveUser() {
+        System.out.println("Welcome to Cerd Bank...");
+        System.out.println("Today is your first time? Press 1 for YES or 2 for NO");
         while (checkCondition) {
-            System.out.println("Welcome to Cerd Bank...");
-            System.out.println("Today is your first time? Press 1 for YES or 2 for NO");
-            receiveOptional = scannerOperations.nextInt();
-            if (receiveOptional == 1) {
-                System.out.println("Below you can see more about us:");
-                System.out.println("----------------------------------------------");
-                System.out.println("Our bank is amid at young people who have started theis adult life and are looking for a place to save their money and invest it.");
-                System.out.println("----------------------------------------------");
-                if (twoFactorValidate()) {
-                    requestCreateAccount();
-                }
-                //return true;
-            } else if (receiveOptional == 2) {
-                System.out.println("F on chat guys...");
-            } else {
-                System.out.println("The value you entered is incompatible... Please try again");
+            Integer receiveOptional = scannerOperations.nextInt();
+            switch (receiveOptional) {
+                case 1:
+                    System.out.println("Below you can see more about us:");
+                    System.out.println("**--------------------------------------------------------------------------------------------------------**");
+                    System.out.println("Our bank is amid at young people who have started theis adult life and are looking for a place to save their money and invest it.");
+                    System.out.println("**--------------------------------------------------------------------------------------------------------**");
+                    if (twoFactorValidate()) {
+                        requestCreateAccount();
+                    }
+                    break;
+                case 2:
+                    System.out.println("Very nice, so I believe you have an account. Enter your CPF so we can check it in the system:");
+                    if (twoFactorValidate()) {
+                        requestCreateAccount();
+                    }
+                    break;
+                default:
+                    System.out.println("The value you entered is incompatible... Please try again with 1 or 2:");
+                    continue;
             }
         }
-        //return false;
     }
 
+    //Atribuir essa função para a classe validateUtil
     private boolean twoFactorValidate() {
         System.out.println("As a precaution, enter your CPF to validate that the account exists in the system:");
         while (checkCondition) {
@@ -59,30 +65,34 @@ public class UserController extends AccountService {
     }
 
     private void requestCreateAccount() {
+        System.out.println("Would you like to create an account? Press 1 for YES or 2 for NO:");
         while (checkCondition) {
-            System.out.println("Would you like to create an account? Press 1 for YES or 2 for NO:");
             receiveOptional = scannerOperations.nextInt();
-            if (receiveOptional == 1) {
-                System.out.println("Please enter your information:");
-                System.out.println("Name:");
-                String name = scannerOperations.next();
-                System.out.println("Age:");
-                int age = Integer.parseInt(scannerOperations.next());
-                System.out.println("CPF:");
-                String cpf = scannerOperations.next();
-                System.out.println("Cell Phone:");
-                String cellphone = scannerOperations.next();
-                if (name != null && age != ' ' && cpf != null && cellphone != null) {
-                    createAccount(name, age, cpf, cellphone);
-                    checkCondition = false;
-                } else {
-                    System.out.println("Invalid values... start the process again.");
-                }
-            } else if (receiveOptional == 2) {
-                System.out.println("Right, so we end up here... Have a nice day!");
-                System.exit(0);
-            } else {
-                System.out.println("The value you entered is incompatible... Please try again");
+            switch (receiveOptional) {
+                case 1:
+                    System.out.println("Please enter your information:");
+                    System.out.println("Name:");
+                    String name = scannerOperations.next();
+                    System.out.println("Age:");
+                    int age = Integer.parseInt(scannerOperations.next());
+                    System.out.println("CPF:");
+                    String cpf = scannerOperations.next();
+                    System.out.println("Cell Phone:");
+                    String cellphone = scannerOperations.next();
+                    if (name != null && age != ' ' && cpf != null && cellphone != null) {
+                        createAccount(name, age, cpf, cellphone);
+                        checkCondition = false;
+                    } else {
+                        System.out.println("Invalid values... start the process again.");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Right, so we end up here... Have a nice day!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("The value you entered is incompatible... Please try again");
+                    break;
             }
         }
     }
